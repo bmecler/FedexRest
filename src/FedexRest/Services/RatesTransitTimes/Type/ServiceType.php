943cae8df209d976e4e415d1f1664ff906d404f2
Brian Mecler <bmecler@gmail.com>
2023-07-24 06:26:31 -0400

    Added service types entity for quotes

---
+++ src/FedexRest/Services/RatesTransitTimes/Type/ServiceType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FedexRest\Services\RatesTransitTimes\Type;
+
+class ServiceType
+{
+    const _FEDEX_GROUND = 'FEDEX_GROUND';
+    const _GROUND_HOME_DELIVERY = 'GROUND_HOME_DELIVERY';
+    const _FIRST_OVERNIGHT = 'FIRST_OVERNIGHT ';
+    const _PRIORITY_OVERNIGHT = 'PRIORITY_OVERNIGHT';
+    const _FEDEX_2_DAY = 'FEDEX_2_DAY ';
+}
