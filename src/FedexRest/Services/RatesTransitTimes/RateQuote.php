898ca49d288852f83abe83a02f9598a83ec4722a
Brian Mecler <bmecler@gmail.com>
2023-07-23 22:39:15 -0400

    Test

--- src/FedexRest/Services/RatesTransitTimes/RateQuote.php
+++ src/FedexRest/Services/RatesTransitTimes/RateQuote.php
@@ -268,10 +268,10 @@ class RateQuotes extends AbstractRequest
     {
         $data = [];
 
-       /* $data["rateRequestControlParameters"]["returnTransitTimes"] = true;
+        $data["rateRequestControlParameters"]["returnTransitTimes"] = true;
         $data["rateRequestControlParameters"]["servicesNeededOnRateFailure"] = true;
         $data["rateRequestControlParameters"]["variableOptions"] = 'FREIGHT_GUARANTEE';
-        $data["rateRequestControlParameters"]["rateSortOrder"] = 'SERVICENAMETRADITIONAL'; */
+        $data["rateRequestControlParameters"]["rateSortOrder"] = 'SERVICENAMETRADITIONAL';
 
         if (!empty($this->account_number)) {
             $data['accountNumber']['value'] = $this->account_number;
@@ -282,16 +282,12 @@ class RateQuotes extends AbstractRequest
         if (!empty($this->recipient)) {
             $data['requestedShipment']['recipient'] = $this->recipient->prepare();
         }
-        /*
-        if (!empty($this->account_number)) {
-            $data['requestedShipment']['serviceType'] = 'STANDARD_OVERNIGHT';
-        }
-        */
+        $data['requestedShipment']['serviceType'] = 'STANDARD_OVERNIGHT';
         if (!empty($this->email_notification_recipient)) {
             $data['requestedShipment']['emailNotificationDetail'] = [$this->email_notification_recipient->prepare()];
         }
-       // $data['requestedShipment']['preferredCurrency'] = 'USD';
-       // $data['requestedShipment']['rateRequestType'] = ['ACCOUNT','LIST'];
+        $data['requestedShipment']['preferredCurrency'] = 'USD';
+        $data['requestedShipment']['rateRequestType'] = ['ACCOUNT','LIST'];
         if (!empty($this->ship_datestamp)) {
             $data['requestedShipment']['shipDateStamp'] = $this->ship_datestamp;
         }
@@ -304,15 +300,15 @@ class RateQuotes extends AbstractRequest
         if (!empty($this->document_shipment)) {
             $data['requestedShipment']['documentShipment'] = $this->document_shipment;
         }
-        //$data['requestedShipment']['packagingType'] = 'YOUR_PACKAGING';
+        $data['requestedShipment']['packagingType'] = 'YOUR_PACKAGING';
         if (!empty($this->total_package_count)) {
             $data['requestedShipment']['totalPackageCount'] = $this->total_package_count;
         }
         if (!empty($this->total_weight)) {
             $data['requestedShipment']['totalWeight'] = $this->total_weight;
         }
-       // $data['requestedShipment']['groupShipment'] = true;
-       // $data['carrierCodes'] = ['FDXE'];
+        $data['requestedShipment']['groupShipment'] = true;
+        $data['carrierCodes'] = ['FDXE'];
 
         return [
             'json' => [$data],
